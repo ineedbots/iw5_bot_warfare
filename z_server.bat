fbaa98e9497461496e5344b3eb400668153d1f55
ineedbots <ineedbots@outlook.com>
2021-09-04 16:34:09 -0600

    config

--- z_server.bat
+++ z_server.bat
@@ -7,14 +7,16 @@ set rcon_password=
 set cfg=server.cfg
 ::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
 set name=IW5 Bot Warfare
-::Port used by the server (default: 4976)
+::Port used by the server (default: 27016)
 set port=27020
 ::What ip to bind too
 set ip=0.0.0.0
 ::Mod name (default "")
-set mod=""
+set mod=
 ::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
 set gamepath=%cd%
+::IMPORTANT! Make sure the filename is unique for each server you clone!
+set g_log=logs\games_mp.log
 
 title PlutoniumIW5 MP - %name% - Server restarter
 echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
@@ -24,6 +26,6 @@ echo (%date%)  -  (%time%) %name% server start.
 
 cd /D %LOCALAPPDATA%\Plutonium
 :server
-start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe iw5mp "%gamepath%" -dedicated +unattended -sv_config "%cfg%" -key "%key%" -net_ip "%ip%" -net_port "%port%" -rcon_password "%rcon_password%"
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe iw5mp "%gamepath%" -dedicated +unattended -sv_config "%cfg%" -key "%key%" -net_ip "%ip%" -net_port "%port%" -rcon_password "%rcon_password%" -fs_game "%mod%" -g_log "%g_log%" +start_map_rotate
 echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
 goto Server
